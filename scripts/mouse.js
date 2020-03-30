69bd98ca965ac48c9c53ec4f50373b6961dbf784
Kay <kay.liang@outlook.com>
2020-03-29 22:21:48 -0400

    adding new files

---
+++ scripts/mouse.js
@@ -0,0 +1,43 @@
+var ines = document.createElement("img");
+ines.setAttribute('src', "images/ines.png");
+ines.setAttribute('onmouseover', "mouseOverInes(this)");
+ines.setAttribute('onmouseout', "mouseOutInes(this)");
+ines.setAttribute('onclick', "onClick()");
+ines.setAttribute('class', "draggable");
+ines.setAttribute('id', "ines");
+ines.setAttribute('style', "width: 30%; position: absolute; bottom: 0; right:10px");
+
+document.body.appendChild(ines);
+
+// var shopItems = document.getElementsByClassName('shop-item')
+// for (item of shopItems){
+//     item.setAttribute('ondblclick', "doubleClick()");
+// }
+function mouseOverInes(img){
+	img.src = "images/ines2.png"
+}
+function mouseOutInes(img){
+	img.src = "images/ines.png"
+}
+function onClick(){
+	window.location.href = "index.html";
+}
+
+$(function() {
+    $( ".draggable" ).draggable();
+  });
+// $('.foreground').mousedown(function(ev) {
+//     $('.background').trigger(ev);
+// });
+// $('.foreground').dblclick(function(ev) {
+//     $('.background').trigger(ev);
+// });
+// $('.foreground').mouseover(function(ev) {
+//     $('.background').trigger(ev);
+// });
+// $('.foreground').mouseout(function(ev) {
+//     $('.background').trigger(ev);
+// });
+// $('.foreground').click(function(ev) {
+//     $('.background').scroll();
+// });
